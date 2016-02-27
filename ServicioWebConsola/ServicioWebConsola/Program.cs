f7450c591493442c6ab2756870b47148b2714b2e
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2016-02-27 13:06:54 +0100

    Añadidos nuevos proyectos

---
+++ ServicioWebConsola/ServicioWebConsola/Program.cs
@@ -0,0 +1,19 @@
+﻿using ServicioWebConsola.ServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioWebConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GlobalWeatherSoap ws = new GlobalWeatherSoapClient("GlobalWeatherSoap");
+            Console.WriteLine(ws.GetWeather("Madrid", "Spain"));
+            Console.ReadLine();
+        }
+    }
+}
